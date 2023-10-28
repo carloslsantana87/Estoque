79498c16158f3e564ea9edb37390388994354087
carloslsantana87 <139463823+carloslsantana87@users.noreply.github.com>
2023-10-28 00:55:36 -0300

    Iniciando TypeScript

--- mensagem.ts
+++ mensagem.ts
@@ -1,1 +1,14 @@
-console.log("Olá mundo!!!");
+enum StatusCodes {
+    NotFound = 404,
+    Success = 200,
+    Accepted = 202,
+    BadRequest = 400
+  }
+  // logs 404
+let soma = StatusCodes.NotFound  + StatusCodes.Success;
+
+  console.log(StatusCodes.NotFound);
+  // logs 200
+  console.log(StatusCodes.Success);
+
+  console.log(soma);
